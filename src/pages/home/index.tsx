64fca76be5056b7e16dc1f383dff3f4d7f680fae
abdulgani88 <abdul.gani@fullertonhealth.com>
2023-07-25 01:36:36 +0700

    home page text with lottie animation

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,15 +1,25 @@
 // import React from 'react'
+import { useLottie } from 'lottie-react';
 import './home.css';
+import animation from '../../assets/animation.json';
 
 export function Home() {
+  const options = {
+    animationData: animation,
+    loop: true,
+    style: { maxWidth: 500, maxHeight: 500 }
+  };
+  const { View } = useLottie(options);
+
   return (
     <section id="home" className="slide-in-element">
       <div className="upper">
         <div className="divide">
           <h1 className="jargon">Lorem. ipsum. dolor. sit amet.</h1>
-          <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente deleniti eos ratione, voluptatum dolores
+          <h3 className='jargon-desc'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente deleniti eos ratione, voluptatum dolores
             doloribus.</h3>
         </div>
+        <>{View}</>
       </div>
 
     </section>
